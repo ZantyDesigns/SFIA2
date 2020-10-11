926d9d32b227fd10b803fbc503290ab7c8845674
Jordan Hamilton <jordanhamilton31@hotmail.com>
2020-10-11 16:24:52 +0100

    Added functionality for Terraform
    Signed-off-by: Jordan Hamilton <jordanhamilton31@hotmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "jenkins" {
      ami = "ami-09a1e275e350acf38"
      instance_type = "t2.micro"
      subnet_id = "subnet-b99dc3d0"
-     vpc_security_group_ids = "sg-0b4002c4c66cab4b4"
+     vpc_security_group_ids = ["sg-0b4002c4c66cab4b4",]
      key_name = "final-instance-1"
 }
 
@@ -16,7 +16,7 @@ resource "aws_instance" "test" {
     ami = "ami-09a1e275e350acf38"
     instance_type = "t2.micro"
     subnet_id = "subnet-b99dc3d0"
-    vpc_security_group_ids = "sg-0b4002c4c66cab4b4"
+    vpc_security_group_ids = ["sg-0b4002c4c66cab4b4",]
     key_name = "test-instance"
 }
 
@@ -24,6 +24,6 @@ resource "aws_instance" "deploy" {
     ami = "ami-09a1e275e350acf38"
     instance_type = "t2.micro"
     subnet_id = "subnet-b99dc3d0"
-    vpc_security_group_ids = "sg-0b4002c4c66cab4b4"
+    vpc_security_group_ids = ["sg-0b4002c4c66cab4b4",]
     key_name = "final-instance-2"
 }
