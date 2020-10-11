16afa1ed9d413d19822514631c9e838cfeacc721
Jordan Hamilton <jordanhamilton31@hotmail.com>
2020-10-12 00:39:34 +0100

    Added Ansible to install Java and Jenkins on jenkins vm
    Signed-off-by: Jordan Hamilton <jordanhamilton31@hotmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,43 @@ resource "aws_instance" "test" {
     vpc_security_group_ids = ["sg-0b4002c4c66cab4b4",]
     key_name = "test-instance"
 }
+
+### The Elastic IP for the Bastion
+resource "aws_eip" "EIP" {
+ vpc = true
+ instance = aws_instance.jenkins.id
+ associate_with_private_ip = aws_instance.jenkins.private_ip
+ tags = {
+  Name = "elastic-ip"
+ }
+}
+
+resource "aws_db_instance" "deploy" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "deploy"
+  db_subnet_group_name = "default-vpc-134e1a7b"
+  vpc_security_group_ids = ["sg-0b4002c4c66cab4b4",]
+  identifier           = "deploydb"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+}
+
+resource "aws_db_instance" "testdb" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "testdb"
+  db_subnet_group_name = "default-vpc-134e1a7b"
+  vpc_security_group_ids = ["sg-0b4002c4c66cab4b4",]
+  identifier           = "testdb"
+  username             = "admin"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,15 @@
 output "ec2_id_jenkins" {
   value = aws_instance.jenkins.id
+}
+
+output "ec2_dns_jenkins" {
   value = aws_instance.jenkins.public_dns
 }
 
 output "ec2_id_test" {
   value = aws_instance.test.id
+}
+
+output "ec2_dns_test" {
   value = aws_instance.test.public_dns
 }
